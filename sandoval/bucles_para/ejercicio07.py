59a6c6de3f71a46b4161661cfd7d251c0ff46808
ELMER015A <56976465+elmer015a@users.noreply.github.com>
2019-11-24 13:33:08 -0500

    se agrego el ejercicio 07 de bucles para

---
+++ sandoval/bucles_para/ejercicio07.py
@@ -0,0 +1,10 @@
+# suma de los 100 primeros numeros impares
+import os
+i=int(os.sys.argv[1])
+suma=0
+while(i<=50):
+    suma += i
+    i += 1
+
+# fin_while
+print("la suma de los 100 primeros numeros impares:", suma)
