d01111454c1fb7be255444b3c530b563ed0b25b1
ELMER015A <56976465+elmer015a@users.noreply.github.com>
2019-11-24 14:13:12 -0500

    se agrego el ejercicio 15 de bucles iterador en rango

---
+++ sandoval/bucles_iterador_rango/ejercicio15.py
@@ -0,0 +1,11 @@
+# Imprimir los numeros del 20 al 100
+import os
+x=int(os.sys.argv[1])
+y=int(os.sys.argv[2])
+
+for i in range(x,y):
+    print(i)
+
+#fin_iterador_en_rango
+
+print("Fin del bucle")
