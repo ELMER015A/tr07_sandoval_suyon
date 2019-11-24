872d9398cda6163edd8895d7ea4a615591217dc9
ELMER015A <56976465+elmer015a@users.noreply.github.com>
2019-11-24 15:04:04 -0500

    se agrego el ejercicio 19 de bucles iterador

---
+++ sandoval/bucles_iterador/ejercicio19.py
@@ -0,0 +1,11 @@
+import os
+msg="RMSF"
+for letra in msg:
+    if letra == "R":
+        print(os.sys.argv[1])
+    if letra == "M":
+        print(os.sys.argv[2])
+    if letra == "S":
+        print(int(os.sys.argv[3]))
+    if letra == "F":
+        print(os.sys.argv[4])
