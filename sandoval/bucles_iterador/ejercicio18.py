74a7d5b0520452a73aaa72c7b4d846eb58b90178
ELMER015A <56976465+elmer015a@users.noreply.github.com>
2019-11-24 15:03:47 -0500

    se agrego el ejercicio 18 de bucles iterador

---
+++ sandoval/bucles_iterador/ejercicio18.py
@@ -0,0 +1,19 @@
+# decodificar el mensaje
+import os
+msg="ABCDEF"
+for letra in msg:
+    if letra == "A":
+        print(os.sys.argv[1])
+    if letra == "B":
+        print(os.sys.argv[2])
+    if letra == "C":
+        print(os.sys.argv[3])
+    if letra == "D":
+        print(os.sys.argv[4])
+    if letra == "E":
+        print(os.sys.argv[5])
+    if letra == "F":
+        print(os.sys.argv[6])
+
+# fin_while
+print("fin del bucle")
