17dd34f6d2a88cabad7be50b3f85b66855c4b6ea
ELMER015A <56976465+elmer015a@users.noreply.github.com>
2019-11-24 15:03:29 -0500

    se agrego el ejercicio 17 de bucles iterador

---
+++ sandoval/bucles_iterador/ejercicio17.py
@@ -0,0 +1,15 @@
+# decodificar los mensajes
+import os
+msg="ABCD"
+for letra in msg:
+    if letra == "A":
+        print(os.sys.argv[1])
+    if letra == "B":
+        print(os.sys.argv[2])
+    if letra == "C":
+        print(os.sys.argv[3])
+    if letra == "D":
+        print(int(os.sys.argv[4]))
+
+# fin_iterador
+print("fin del bucle")
