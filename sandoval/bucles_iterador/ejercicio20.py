e461862011b8a8e6e03f23db03df7d19ce038b88
ELMER015A <56976465+elmer015a@users.noreply.github.com>
2019-11-24 15:04:23 -0500

    se agrego el ejercicio 20e de bucles iterador

---
+++ sandoval/bucles_iterador/ejercicio20.py
@@ -0,0 +1,15 @@
+import os
+msg="ABCDEF"
+for letra in msg:
+    if letra == "A":
+        print(os.sys.argv[1])
+    if letra == "B":
+        print(os.sys.argv[2])
+    if letra == "C":
+        print(os.sys.argv[3])
+    if letra == "D":
+        print(os.sys.argv[4])
+    if letra == "E":
+        print(os.sys.argv[5])
+    if letra == "F":
+        print(float(os.sys.argv[6]))
