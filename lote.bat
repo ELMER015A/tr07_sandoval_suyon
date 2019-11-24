4a4cbffe0d9febabfa1c0b787991ce26a6aa3121
ELMER015A <56976465+elmer015a@users.noreply.github.com>
2019-11-24 15:04:48 -0500

    se agrego el lote bat

---
+++ lote.bat
@@ -0,0 +1,45 @@
+python sandoval/validador_datos/ejercicio01.py 9.8
+
+python sandoval/validador_datos/ejercicio02.py 50
+
+python sandoval/validador_datos/ejercicio03.py 1000
+
+python sandoval/validador_datos/ejercicio04.py 600
+
+python sandoval/validador_datos/ejercicio05.py 40
+
+
+
+python sandoval/bucles_para/ejercicio06.py 1
+
+python sandoval/bucles_para/ejercicio07.py 1
+
+python sandoval/bucles_para/ejercicio08.py 20
+
+python sandoval/bucles_para/ejercicio09.py 0
+
+python sandoval/bucles_para/ejercicio10.py 0
+
+
+
+python sandoval/bucles_iterador_rango/ejercicio11.py 80
+
+python sandoval/bucles_iterador_rango/ejercicio12.py 1
+
+python sandoval/bucles_iterador_rango/ejercicio13.py 5 25
+
+python sandoval/bucles_iterador_rango/ejercicio14.py 45 236
+
+python sandoval/bucles_iterador_rango/ejercicio15.py 20 100
+
+
+python sandoval/bucles_iterador/ejercicio16.py ABCD
+
+python sandoval/bucles_iterador/ejercicio17.py cuanto cuesta eseo 9
+
+python sandoval/bucles_iterador/ejercicio18.py amiga que tal sigues estudiando contabilidad
+
+python sandoval/bucles_iterador/ejercicio19.py hijo compra 4 pan
+
+python sandoval/bucles_iterador/ejercicio20.py jose lego la cuenta es 25.4
+
